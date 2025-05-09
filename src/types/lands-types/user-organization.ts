cad6d11fd1e9ebc4c0283382ca51010c4773efa3
Jervx <louellagracechua@gmail.com>
2025-05-09 16:51:53 +0800

    chore: Add missing attributes of IOrganization interface

--- src/types/lands-types/user-organization.ts
+++ src/types/lands-types/user-organization.ts
@@ -1,13 +1,22 @@
+import { IBranch } from '../coop-types'
 import { IUserBase } from '../auth/user'
-import { ITimeStamps, TEntityId, IAuditable } from '../common'
 import { IOrganization } from './organization'
+import { ITimeStamps, TEntityId, IAuditable } from '../common'
 
 export interface IUserOrganization extends ITimeStamps, IAuditable {
     id: TEntityId
 
     organization_id: TEntityId
-    organization: IOrganization
+    organization?: IOrganization
+
+    branchId: string
+    branch?: IBranch
+
+    description?: string
 
     user_id: TEntityId
     user: IUserBase
+
+    applicationDescription?: string
+    applicationStatus: 'pending' | 'reported' | 'accepted' | 'ban'
 }
