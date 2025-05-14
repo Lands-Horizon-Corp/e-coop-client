96ee4ac39bf3ccfdf6052115d0dc32ff56b18ac8
Jervx <louellagracechua@gmail.com>
2025-05-14 18:16:52 +0800

    fix(components): Fix build errors components

--- src/components/ui/date-input-field.tsx
+++ src/components/ui/date-input-field.tsx
@@ -1,26 +1,16 @@
-import { parseDate, CalendarDate } from '@internationalized/date'
+import { CalendarDate } from '@internationalized/date'
 import { DateField, DateInput, DateSegment } from 'react-aria-components'
 
 import { cn } from '@/lib/utils'
 
 type Props = {
-    value?: CalendarValue | undefined
-    onChange: (val?: string) => void
+    value?: CalendarDate | undefined
+    onChange: (val: CalendarDate | null) => void
 }
 
 export function DateInputField({ value, onChange }: Props) {
-    const dateValue: CalendarDate | undefined = value
-        ? parseDate(value)
-        : undefined
-
     return (
-        <DateField
-            value={dateValue}
-            onChange={(val: CalendarDate | null) => {
-                onChange(val?.toString()) // "YYYY-MM-DD" or undefined
-            }}
-            className="w-full"
-        >
+        <DateField value={value} onChange={onChange} className="w-full">
             <DateInput className="flex w-full items-center rounded-md border border-input bg-background px-3 py-2 text-sm shadow-sm ring-offset-background transition-colors focus-within:outline-none focus-within:ring-2 focus-within:ring-ring focus-within:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50">
                 {(segment) => (
                     <DateSegment
