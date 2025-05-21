77c849746148f65c26d2b38261e15c6bdd7d0661
Jervx <louellagracechua@gmail.com>
2025-05-21 19:51:10 +0800

    chore: Add view employee table in view employee page

--- src/routes/org/$orgname/branch.$branchname/(maintenance)/(employees)/employees/view-employees.tsx
+++ src/routes/org/$orgname/branch.$branchname/(maintenance)/(employees)/employees/view-employees.tsx
@@ -1,3 +1,6 @@
+import PageContainer from '@/components/containers/page-container'
+import EmployeesTable from '@/components/tables/employees-table'
+import EmployeesAction from '@/components/tables/employees-table/action'
 import { createFileRoute } from '@tanstack/react-router'
 
 export const Route = createFileRoute(
@@ -7,10 +10,25 @@ export const Route = createFileRoute(
 })
 
 function RouteComponent() {
+    // const [createModal, setCreateModal] = useState(false)
+
     return (
-        <div>
-            Hello
-            "/org/$orgname/branch/$branchname/(maintenance)/users/employee/"!
-        </div>
+        <PageContainer>
+            {/* <MemberProfileQuickCreateFormModal
+                open={createModal}
+                onOpenChange={setCreateModal}
+                formProps={{
+                    defaultValues: {
+                        // TODO: Once org was established, and branch id exist, put it here
+                    },
+                    onSuccess: () => {},
+                }}
+            /> */}
+            <EmployeesTable
+                actionComponent={(props) => <EmployeesAction {...props} />}
+                toolbarProps={{}}
+                className="max-h-[90vh] min-h-[90vh] w-full"
+            />
+        </PageContainer>
     )
 }
