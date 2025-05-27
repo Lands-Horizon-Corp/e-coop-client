422c9c192b852d001a69c0fa424cde10e2943acf
Jervx <louellagracechua@gmail.com>
2025-05-27 15:40:29 +0800

    feat(data-layer/batch-cash-count): Add batch cash count related data layers

---
+++ src/hooks/use-form-helper.ts
@@ -0,0 +1,63 @@
+import {
+    Path,
+    FieldValues,
+    UseFormReturn,
+    DefaultValues,
+} from 'react-hook-form'
+import { useCallback, useEffect } from 'react'
+
+import useConfirmModalStore from '@/store/confirm-modal-store'
+
+interface UseFormHelperProps<T, TForm extends FieldValues> {
+    readOnly?: boolean
+    form: UseFormReturn<TForm>
+    hiddenFields?: Array<Path<T>>
+    resetOnDefaultChange?: boolean
+    disabledFields?: Array<Path<T>>
+    defaultValues?: DefaultValues<T>
+    onNewDefaulValueNotice?: { title: string; description: string }
+}
+
+export const useFormHelper = <T, TForm extends FieldValues>({
+    readOnly = false,
+    hiddenFields = [],
+    disabledFields = [],
+    form,
+    defaultValues,
+    resetOnDefaultChange = false,
+}: UseFormHelperProps<T, TForm>) => {
+    const { onOpen } = useConfirmModalStore()
+
+    useEffect(() => {
+        if (resetOnDefaultChange && defaultValues) {
+            if (form.formState.isDirty) {
+                onOpen({
+                    title: 'Data Changed',
+                    description:
+                        'The data you are currently editting has changed, but you have unsaved changes. Do you want to keep editting or update to newest data?',
+                    confirmString: 'Newest',
+                    cancelString: 'Keep Editting',
+                    onConfirm: () =>
+                        form.reset(defaultValues as unknown as TForm),
+                })
+            } else {
+                form.reset(defaultValues as TForm)
+            }
+        }
+    }, [form, onOpen, defaultValues, resetOnDefaultChange])
+
+    const isDisabled = useCallback(
+        (field: Path<T>) => readOnly || disabledFields.includes(field),
+        [readOnly, disabledFields]
+    )
+
+    const isHidden = useCallback(
+        (field: Path<T>) => hiddenFields.includes(field),
+        [hiddenFields]
+    )
+
+    return {
+        isHidden,
+        isDisabled,
+    }
+}
