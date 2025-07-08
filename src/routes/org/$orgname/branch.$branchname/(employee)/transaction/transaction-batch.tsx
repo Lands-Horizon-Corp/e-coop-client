81cf93074c61c7341cdbd62d734eb9f21795a4f9
Jervx <louellagracechua@gmail.com>
2025-07-08 11:03:09 +0800

    chore: Add transaction batch table routes

--- src/routes/org/$orgname/branch.$branchname/(employee)/transaction/transaction-batch.tsx
+++ src/routes/org/$orgname/branch.$branchname/(employee)/transaction/transaction-batch.tsx
@@ -1,4 +1,14 @@
+import { useQueryClient } from '@tanstack/react-query'
+
+import { useAuthUserWithOrgBranch } from '@/store/user-auth-store'
 import { createFileRoute } from '@tanstack/react-router'
+
+import PageContainer from '@/components/containers/page-container'
+import TransactionBatchTable from '@/components/tables/transaction-batch-table'
+import TransactionBatchTableAction from '@/components/tables/transaction-batch-table/action'
+
+import { useModalState } from '@/hooks/use-modal-state'
+import { useSubscribe } from '@/hooks/use-pubsub'
 
 export const Route = createFileRoute(
     '/org/$orgname/branch/$branchname/(employee)/transaction/transaction-batch'
@@ -7,10 +17,46 @@ export const Route = createFileRoute(
 })
 
 function RouteComponent() {
+    const createModal = useModalState()
+    const {
+        currentAuth: {
+            user_organization: { branch_id },
+        },
+    } = useAuthUserWithOrgBranch()
+
+    const queryClient = useQueryClient()
+
+    useSubscribe(`transaction_batch.created.branch.${branch_id}`, () => {
+        queryClient.invalidateQueries({
+            queryKey: ['transaction-batch', 'resource-query'],
+        })
+    })
+
+    useSubscribe(`transaction_batch.updated.branch.${branch_id}`, () => {
+        queryClient.invalidateQueries({
+            queryKey: ['transaction-batch', 'resource-query'],
+        })
+    })
+
+    useSubscribe(`transaction_batch.deleted.branch.${branch_id}`, () => {
+        queryClient.invalidateQueries({
+            queryKey: ['transaction-batch', 'resource-query'],
+        })
+    })
+
     return (
-        <div>
-            Hello
-            "/org/$orgname/branch/$branchname/(employee)/transaction/transaction-batch"!
-        </div>
+        <PageContainer>
+            <TransactionBatchTable
+                toolbarProps={{
+                    createActionProps: {
+                        onClick: () => createModal.onOpenChange(true),
+                    },
+                }}
+                actionComponent={(prop) => (
+                    <TransactionBatchTableAction {...prop} />
+                )}
+                className="max-h-[90vh] min-h-[90vh] w-full"
+            />
+        </PageContainer>
     )
 }
