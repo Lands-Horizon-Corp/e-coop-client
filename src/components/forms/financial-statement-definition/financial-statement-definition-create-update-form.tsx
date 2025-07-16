19b457c4f3ad6f9315aeea2dbb8126784bdccfbf
dctrrohan <dctrrohan@gmail.com>
2025-07-16 09:19:31 +0800

    feat/chore: fs definition and refactor gl state

--- src/components/forms/financial-statement-definition/financial-statement-definition-create-update-form.tsx
+++
@@ -1,6 +0,0 @@
-const FinancialStatementCreateUpdateForm = () => {
-    //todo: Implement the form logic for creating or updating a financial statement definition
-    return <div></div>
-}
-
-export default FinancialStatementCreateUpdateForm
