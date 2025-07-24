fae1555555a645b0044556d406f4fc722540b7af
z-dayao <zalvendayao888@gmail.com>
2025-07-24 19:14:19 +0800

    feat: galaxy ui

---
+++ src/components/ui/galaxy/index.tsx
@@ -0,0 +1,37 @@
+'use client'
+
+import type React from 'react'
+import { useRef } from 'react'
+
+import { Canvas } from '@react-three/fiber'
+
+import { Galaxy } from './galaxy'
+
+export function Galaxy3D() {
+    const mouse = useRef({ x: 0, y: 0 })
+
+    const handleMouseMove = (event: React.MouseEvent) => {
+        mouse.current.x = (event.clientX / window.innerWidth) * 2 - 1
+        mouse.current.y = -(event.clientY / window.innerHeight) * 2 + 1
+    }
+
+    return (
+        <div
+            className="fixed inset-0 -z-10 bg-gray-950"
+            onMouseMove={handleMouseMove}
+        >
+            <Canvas
+                camera={{
+                    position: [1, 2, 3],
+                    fov: 75,
+                }}
+                gl={{
+                    alpha: true,
+                    antialias: true,
+                }}
+            >
+                <Galaxy mouseRef={mouse} />
+            </Canvas>
+        </div>
+    )
+}
