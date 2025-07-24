955139a52955613271ad42d33650ed47e07ac5ad
Jervx <louellagracechua@gmail.com>
2025-07-24 12:23:09 +0800

    fix(components): Fix transaction batch form to accept 0 amount on batch fund

--- src/components/forms/transaction-batch-forms/transaction-batch-create-form.tsx
+++ src/components/forms/transaction-batch-forms/transaction-batch-create-form.tsx
@@ -36,13 +36,15 @@ import {
 
 const transactionBatchSchema = z.object({
     name: z.string().min(1, 'Name is required'),
-    amount: z.coerce.number().min(0.01, 'Amount is required'),
+    amount: z.coerce.number().min(0, 'Amount is required'),
     description: descriptionSchema
         .optional()
         .transform(descriptionTransformerSanitizer),
     organization_id: z.string().optional(),
     branch_id: z.string().optional(),
-    provided_by_user_id: entityIdSchema.min(1, 'Provider is required'),
+    provided_by_user_id: entityIdSchema
+        .min(1, 'Provider is required')
+        .optional(),
     provided_by_user: z.any(),
     signature_media_id: z.string().optional(),
     signature_media: z.any(),
