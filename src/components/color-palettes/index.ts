ba83e66b16198e6c65bfc4bd2a41ee7e123103fa
Jervx <louellagracechua@gmail.com>
2025-07-28 18:52:45 +0800

    fix(data-layer): Remove components from data layer

--- src/components/color-palettes/index.ts
+++
@@ -1,1 +0,0 @@
-export const blueGradientPalette = ['#0A1D36', '#133C6B', '#3B7EC0', '#5C9CDC']
--- src/components/developer/api-key-gen.tsx
+++
@@ -1,9 +0,0 @@
-// import { IClassProps } from '@/types'
-
-// interface Props extends IClassProps {}
-
-const APIKeyGen = () => {
-    return <div>APIKeyGen</div>
-}
-
-export default APIKeyGen
