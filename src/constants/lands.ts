784cb8ddcaf9a15e882110851a21388d9fe0ed25
Jervx <louellagracechua@gmail.com>
2025-07-28 12:57:37 +0800

    feat(data-layer): Add constants and types related to ecoop lands team members

---
+++ src/constants/lands.ts
@@ -0,0 +1,50 @@
+import { LandsTeamMember } from '@/types'
+
+export const LANDS_TEAM: LandsTeamMember[] = [
+    {
+        name: 'Zalven Lemuel S. Dayao',
+        image: '/pictures/team/zalven.webp',
+        position: 'CEO & Founder',
+        description: "Principal architect driving the company's technological direction. Oversees the design and optimization of backend infrastructures, ensures robust database management systems, and spearheads initiatives for high-performance, scalable, and secure systems across the organization.",
+        linkedInUrl: 'https://www.linkedin.com/in/zalven-dayao-293b2a22a/',
+        instagramUrl: 'https://www.instagram.com/zalven_blue/',
+    },
+    {
+        name: 'Nelma Dayao',
+        image: '/pictures/team/nelma.webp',
+        position: 'President & Co-Founder',
+        description: 'Dynamic executive and project management leader. Orchestrates the development and execution of strategic initiatives, particularly the cooperative project. Focuses on operational excellence, team synergy, and the realization of long-term company goals.',
+        facebookUrl: 'https://www.facebook.com/nelma.dayao',
+    },
+    {
+        name: 'Sajiropn Dayao',
+        image: '/pictures/team/vpsanty.webp',
+        position: 'CFO (Chief Financial Officer) & Investor',
+        description: "Financial strategist and key investor ensuring the company's fiscal health and growth. Leads marketing initiatives, provides life coaching for team development, and oversees funding strategies and business sustainability.",
+        facebookUrl: 'https://www.facebook.com/vpsantyofficial',
+    },
+    {
+        name: 'Danilo Dayao',
+        image: '/pictures/team/nelma.webp',
+        position: 'Co-Founder',
+        description: 'Dynamic executive and project management leader. Orchestrates the development and execution of strategic initiatives, particularly the cooperative project. Focuses on operational excellence, team synergy, and the realization of long-term company goals.',
+        facebookUrl: 'https://www.facebook.com/nelma.dayao',
+    },
+    {
+        name: 'Jerbee Paragas',
+        image: '/pictures/team/jerbee.webp',
+        position: 'CTO (Chief Technology Officer)',
+        description: 'Technical strategist and senior frontend engineering expert. Leads API integration processes, enhances application performance, and architects frontend systems with modern frameworks to deliver seamless, responsive, and highly optimized user experiences.',
+        linkedInUrl: 'https://www.linkedin.com/in/jerbee-paragas-8970b024b/',
+        instagramUrl: 'https://www.instagram.com/jerbcxz/',
+    },
+    {
+        name: 'Rojan Yepes',
+        image: '/pictures/team/rojan.webp',
+        position: 'CTO (Chief Technology Officer)',
+        description: 'Creative technologist and UI/UX leader. Heads the frontend team to craft innovative, user-centric designs while aligning visual identity with functional requirements. Drives UI/UX strategies ensuring intuitive and engaging digital experiences.',
+        linkedInUrl: 'https://www.linkedin.com/in/rojan-yepes-5a8395254/',
+        instagramUrl:
+            'https://www.instagram.com/rojanyepes?utm_source=ig_web_button_share_sheet&igsh=MWFyYjIwYjZvb3gzMg==',
+    },
+]
