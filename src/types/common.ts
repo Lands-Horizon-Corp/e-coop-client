6ba41065f2db0ffcc8bcf19617b461809764ab0a
Jervx <louellagracechua@gmail.com>
2025-08-19 10:17:48 +0800

    chore: Add common validation and common types

---
+++ src/types/common.ts
@@ -0,0 +1,121 @@
+import {
+    AccountClosureReasons,
+    CIVIL_STATUS,
+    EDUCATIONAL_ATTAINMENT,
+    FAMILY_RELATIONSHIP,
+    GENERAL_STATUS,
+    TAG_CATEGORY,
+    USER_TYPE,
+} from '@/constants'
+import {
+    COMPUTATION_TYPE,
+    LOAN_AMORTIZATION_TYPE,
+    LOAN_COLLECTOR_PLACE,
+    LOAN_COMAKER_TYPE,
+    LOAN_MODE_OF_PAYMENT,
+    LOAN_TYPE,
+    WEEKDAYS,
+} from '@/constants/loan'
+
+import { IOrganization } from '../modules/organization'
+import { IUserBase } from '../modules/user/user.types'
+
+export type TEntityId = string
+
+export type TUserType = (typeof USER_TYPE)[number]
+
+export type TGeneralStatus = (typeof GENERAL_STATUS)[number]
+
+export type TRelationship = (typeof FAMILY_RELATIONSHIP)[number]
+
+export type TTagCategory = (typeof TAG_CATEGORY)[number]
+
+export interface ILongLat {
+    longitude?: number
+    latitude?: number
+}
+
+/* Extend interface if gusto magka ts type neto */
+export interface IAuditable {
+    created_by_id?: TEntityId
+    created_by?: IUserBase
+
+    updated_by_id?: TEntityId
+    updated_by?: IUserBase
+
+    deleted_by_id?: TEntityId
+    deleted_by?: IUserBase
+}
+
+/* Only use this for entity that has branch_id */
+export interface IIDentity {
+    branch_id: TEntityId
+    // branch: IBranch
+}
+
+export interface IOrgIdentity {
+    organization_id: TEntityId
+    organization: IOrganization
+}
+
+/* Identity of the entity */
+export interface IOrgBranchIdentity {
+    organization_id: TEntityId
+    organization: IOrganization
+
+    branch_id: TEntityId
+    // branch: IBranch
+}
+
+/* Use this only if entity has timestamps, auditable, and has org and branch */
+export interface IBaseEntityMeta
+    extends ITimeStamps,
+        IAuditable,
+        IOrgBranchIdentity {
+    id: TEntityId
+}
+
+export interface ITimeStamps {
+    deleted_at?: string | undefined
+    created_at: string
+    updated_at?: string
+}
+
+export type TCivilStatus = (typeof CIVIL_STATUS)[number]
+
+export type TAccountClosureReasonType = (typeof AccountClosureReasons)[number]
+
+export type TEducationalAttainment = (typeof EDUCATIONAL_ATTAINMENT)[number]
+
+export interface UpdateIndexRequest {
+    id: TEntityId
+    index: number
+}
+
+interface IPages {
+    page: string
+    pageIndex: string
+}
+
+export interface IPaginatedResult<T> {
+    data: T[]
+    pageIndex: number
+    totalPage: number
+    pageSize: number
+    totalSize: number
+    pages: IPages[]
+}
+
+export type TLoanModeOfPayment = (typeof LOAN_MODE_OF_PAYMENT)[number]
+
+export type TWeekdays = (typeof WEEKDAYS)[number]
+
+export type TLoanCollectorPlace = (typeof LOAN_COLLECTOR_PLACE)[number]
+
+export type TLoanComakerType = (typeof LOAN_COMAKER_TYPE)[number]
+
+export type TLoanType = (typeof LOAN_TYPE)[number]
+
+export type TLoanAmortizationType = (typeof LOAN_AMORTIZATION_TYPE)[number]
+
+export type TComputationType = (typeof COMPUTATION_TYPE)[number]
---
+++ src/validation/validation-fn.ts
@@ -0,0 +1,9 @@
+// this files contains custom function that is used for custom validation like transform, etc..
+import { sanitizeHtml } from "@/helpers/sanitizer"
+
+export const descriptionTransformerSanitizer = <T>(val: T) => {
+    if (typeof val === 'string') {
+        return sanitizeHtml(val)
+    }
+    return val
+}
