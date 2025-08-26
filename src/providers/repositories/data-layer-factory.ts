e0db244b00416a2173bd7a186d3cffa88dbad4e4
Jervx <louellagracechua@gmail.com>
2025-08-26 11:33:44 +0800

    refactor: Refactor and added mutation factory Pushed only the new factory for other dev to use

--- src/providers/repositories/data-layer-factory.ts
+++ src/providers/repositories/data-layer-factory.ts
@@ -1,15 +1,14 @@
-import {
-    type MutationOptions,
-    type QueryObserverOptions,
-    useMutation,
-    useQuery,
-} from '@tanstack/react-query'
-import { useQueryClient } from '@tanstack/react-query'
+import { type QueryObserverOptions, useQuery } from '@tanstack/react-query'
 
 import type { TAPIQueryOptions } from '@/types/api'
 import type { IPaginatedResult, TEntityId } from '@/types/common'
 
 import { createAPIRepository } from './api-crud-factory'
+import {
+    createMutationFactory,
+    deleteMutationInvalidationFn,
+    updateMutationInvalidationFn,
+} from './mutation-factory'
 
 export type HookQueryOptions<
     TQueryFnData,
@@ -20,14 +19,11 @@ export type HookQueryOptions<
     'queryKey' | 'queryFn'
 >
 
-export type HookMutationOptions<
-    TQueryFnData,
-    TError = Error,
-    TData = TQueryFnData,
-> = Omit<MutationOptions<TQueryFnData, TError, TData>, 'mutationFn'>
-
 // Create and Returns api base crud service and the query hook version of it
-export const createDataLayerFactory = <TResponse, TRequest>({
+export const createDataLayerFactory = <
+    TResponse extends { id: TEntityId },
+    TRequest,
+>({
     url,
     baseKey,
 }: {
@@ -78,109 +74,48 @@ export const createDataLayerFactory = <TResponse, TRequest>({
         })
     }
 
-    const useCreate = ({
-        options,
-    }: {
-        options?: HookMutationOptions<TResponse, Error, TRequest>
-    } = {}) => {
-        return useMutation<TResponse, Error, TRequest>({
-            ...options,
-            meta: options?.meta
-                ? options.meta
-                : {
-                      invalidates: [
-                          [baseKey, 'paginated'],
-                          [baseKey, 'all'],
-                      ],
-                  },
-            mutationFn: (payload) => baseAPI.create({ payload }),
-        })
-    }
+    const useCreate = createMutationFactory<TResponse, Error, TRequest>({
+        mutationFn: (payload) => baseAPI.create({ payload }),
+        defaultInvalidates: [
+            [baseKey, 'paginated'],
+            [baseKey, 'all'],
+        ],
+    })
 
-    const useUpdateById = ({
-        options,
-    }: {
-        options?: HookMutationOptions<
-            TResponse,
-            Error,
-            { id: TEntityId; payload: TRequest }
-        >
-    } = {}) => {
-        const queryClient = useQueryClient()
+    const useUpdateById = createMutationFactory<
+        TResponse,
+        Error,
+        { id: TEntityId; payload: TRequest }
+    >({
+        mutationFn: (variables) => baseAPI.updateById(variables),
+        // defaultInvalidates: [ // experimentuhan ko pa to
+        //     [baseKey, 'paginated'],
+        //     [baseKey, 'all'],
+        // ],
+        invalidationFn: (args) => updateMutationInvalidationFn(baseKey, args),
+    })
 
-        return useMutation<
-            TResponse,
-            Error,
-            { id: TEntityId; payload: TRequest }
-        >({
-            ...options,
-            meta: options?.meta
-                ? options.meta
-                : {
-                      invalidates: [
-                          [baseKey, 'paginated'],
-                          [baseKey, 'all'],
-                      ],
-                  },
-            mutationFn: (payload) => baseAPI.updateById(payload),
-            onSuccess: async (data, variables, context) => {
-                await Promise.all([
-                    queryClient.invalidateQueries({
-                        queryKey: [baseKey, variables.id],
-                    }),
-                ])
-                options?.onSuccess?.(data, variables, context)
-            },
-        })
-    }
+    const useDeleteById = createMutationFactory<void, Error, TEntityId>({
+        mutationFn: (id) => baseAPI.deleteById({ id }),
+        // defaultInvalidates: [ // experimentuhan ko pa to
+        //     [baseKey, 'paginated'],
+        //     [baseKey, 'all'],
+        // ],
+        invalidationFn: (args) => deleteMutationInvalidationFn(baseKey, args),
+    })
 
-    const useDeleteById = ({
-        options,
-    }: {
-        options?: HookMutationOptions<void, Error, TEntityId>
-    } = {}) => {
-        const queryClient = useQueryClient()
-
-        return useMutation<void, Error, TEntityId>({
-            ...options,
-            meta: options?.meta
-                ? options.meta
-                : {
-                      invalidates: [
-                          [baseKey, 'paginated'],
-                          [baseKey, 'all'],
-                      ],
-                  },
-            mutationFn: (payload) => baseAPI.deleteById({ id: payload }),
-            onSuccess: async (data, variables, context) => {
-                await Promise.all([
-                    queryClient.invalidateQueries({
-                        queryKey: [baseKey, variables],
-                    }),
-                ])
-                options?.onSuccess?.(data, variables, context)
-            },
-        })
-    }
-
-    const useDeleteMany = ({
-        options,
-    }: {
-        options?: HookMutationOptions<void, Error, { ids: TEntityId[] }>
-    } = {}) => {
-        return useMutation<void, Error, { ids: TEntityId[] }>({
-            ...options,
-            meta: options?.meta
-                ? options.meta
-                : {
-                      invalidates: [
-                          [baseKey, 'paginated'],
-                          [baseKey, 'all'],
-                      ],
-                  },
-            mutationFn: (payload) => baseAPI.deleteMany(payload),
-        })
-    }
+    const useDeleteMany = createMutationFactory<
+        void,
+        Error,
+        { ids: TEntityId[] }
+    >({
+        mutationFn: (payload) => baseAPI.deleteMany(payload),
+        // defaultInvalidates: [ // experimentuhan ko pa to
+        //     [baseKey, 'paginated'],
+        //     [baseKey, 'all'],
+        // ],
+        invalidationFn: (args) => deleteMutationInvalidationFn(baseKey, args),
+    })
 
     return {
         baseQueryKey: baseKey,
