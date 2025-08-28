0e4e81e78a62749164cc7221f197d30e8050ea1d
Jervx <louellagracechua@gmail.com>
2025-08-28 15:20:05 +0800

    chore: Add bills and coins route

--- src/routes/org/$orgname/branch.$branchname/(maintenance)/maintenance/bills-and-coins.tsx
+++ src/routes/org/$orgname/branch.$branchname/(maintenance)/maintenance/bills-and-coins.tsx
@@ -1,70 +1,9 @@
-import { useQueryClient } from '@tanstack/react-query'
 import { createFileRoute } from '@tanstack/react-router'
 
-import { useAuthUserWithOrgBranch } from '@/modules/authentication/authgentication.store'
-import { BillsAndCoinCreateUpdateFormModal } from '@/modules/bill-and-coins/components/bills-and-coin-create-update-form'
-import BillsAndCoinsTable from '@/modules/bill-and-coins/components/bills-and-coins-table'
-import BillsAndCoinsAction from '@/modules/bill-and-coins/components/bills-and-coins-table/row-action-context'
-
-import PageContainer from '@/components/containers/page-container'
-
-import { useModalState } from '@/hooks/use-modal-state'
-import { useSubscribe } from '@/hooks/use-pubsub'
+import BillsAndCoinsPage from '@/modules/bill-and-coins/components/pages/bills-and-coins-page'
 
 export const Route = createFileRoute(
     '/org/$orgname/branch/$branchname/(maintenance)/maintenance/bills-and-coins'
 )({
-    component: RouteComponent,
+    component: BillsAndCoinsPage,
 })
-
-function RouteComponent() {
-    const queryClient = useQueryClient()
-    const createModal = useModalState(false)
-    const {
-        currentAuth: {
-            user_organization: { branch_id, organization_id },
-        },
-    } = useAuthUserWithOrgBranch()
-
-    useSubscribe(`bills_and_coins.created.branch.${branch_id}`, () =>
-        queryClient.invalidateQueries({
-            queryKey: ['bills-and-coins', 'paginated'],
-        })
-    )
-
-    useSubscribe(`bills_and_coins.updated.branch.${branch_id}`, () =>
-        queryClient.invalidateQueries({
-            queryKey: ['bills-and-coins', 'paginated'],
-        })
-    )
-
-    useSubscribe(`bills_and_coins.deleted.branch.${branch_id}`, () =>
-        queryClient.invalidateQueries({
-            queryKey: ['bills-and-coins', 'paginated'],
-        })
-    )
-
-    return (
-        <PageContainer>
-            <BillsAndCoinCreateUpdateFormModal
-                {...createModal}
-                formProps={{
-                    defaultValues: {
-                        branch_id,
-                        organization_id,
-                    },
-                    onSuccess: () => {},
-                }}
-            />
-            <BillsAndCoinsTable
-                actionComponent={(props) => <BillsAndCoinsAction {...props} />}
-                toolbarProps={{
-                    createActionProps: {
-                        onClick: () => createModal.onOpenChange(true),
-                    },
-                }}
-                className="max-h-[90vh] min-h-[90vh] w-full"
-            />
-        </PageContainer>
-    )
-}
