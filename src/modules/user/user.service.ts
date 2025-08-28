e94ad5ea6c06f7a9a2f2986508e563f12d221a01
Jervx <louellagracechua@gmail.com>
2025-08-28 15:11:41 +0800

    chore: Modified user service

--- src/modules/user/user.service.ts
+++ src/modules/user/user.service.ts
@@ -1,5 +1,13 @@
-import { createDataLayerFactory } from '@/providers/repositories/data-layer-factory'
+import { useQuery } from '@tanstack/react-query'
 
+import {
+    HookQueryOptions,
+    createDataLayerFactory,
+} from '@/providers/repositories/data-layer-factory'
+
+import type { TEntityId } from '@/types'
+
+import { IMedia } from '../media'
 import type { IUserBase } from './user.types'
 
 const { apiCrudHooks, apiCrudService } = createDataLayerFactory<
@@ -23,3 +31,25 @@ export const {
     useGetPaginated,
     useUpdateById,
 } = apiCrudHooks
+
+// API function to fetch user media
+export const getUserMedias = async (userId: TEntityId) => {
+    const res = await UserAPI.API.get<IMedia[]>(`/api/v1/user/${userId}/medias`)
+    return res.data
+}
+
+// Query hook for fetching user media
+export const useUserMedias = ({
+    userId,
+    options,
+}: {
+    userId: TEntityId
+    options?: HookQueryOptions<IMedia[], Error>
+}) => {
+    return useQuery<IMedia[], Error>({
+        retry: 0,
+        ...options,
+        queryKey: ['user', userId, 'medias'],
+        queryFn: async () => getUserMedias(userId),
+    })
+}
