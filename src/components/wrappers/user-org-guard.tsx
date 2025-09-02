dbfa061efad24021cb6a411c2261fb1b437fc424
Jervx <louellagracechua@gmail.com>
2025-09-02 10:54:08 +0800

    fix: Infinite & toast

--- src/components/wrappers/user-org-guard.tsx
+++ src/components/wrappers/user-org-guard.tsx
@@ -1,45 +1,46 @@
+import { useEffect } from 'react'
+
 import { Navigate } from '@tanstack/react-router'
-// import { useEffect } from 'react'
 import { toast } from 'sonner'
 
 import { useAuthUser } from '@/modules/authentication/authgentication.store'
-
-// import {
-//     useSendHeartbeatOffline,
-//     useSendHeartbeatOnline,
-// } from '@/modules/heartbeat/heartbeat.service'
+import {
+    useSendHeartbeatOffline,
+    useSendHeartbeatOnline,
+} from '@/modules/heartbeat/heartbeat.service'
 
 import { IChildProps } from '@/types'
 
 interface Props extends IChildProps {}
 
 const UserOrgGuard = ({ children }: Props) => {
-    // const sendOnline = useSendHeartbeatOnline()
-    // const sendOffline = useSendHeartbeatOffline()
+    const sendOnline = useSendHeartbeatOnline()
+    const sendOffline = useSendHeartbeatOffline()
 
     const {
         currentAuth: { user_organization },
     } = useAuthUser()
 
-    // useEffect(() => auth{
-    //     // Only start heartbeat if user_organization exists
-    //     if (!user_organization) return
+    useEffect(() => {
+        // Only start heartbeat if user_organization exists
+        if (!user_organization) return
 
-    //     // Send online heartbeat on mount and every 5s
-    //     const sendHeartbeat = () => sendOnline.mutate()
-    //     sendHeartbeat()
+        // Send online heartbeat on mount and every 5s
+        const sendHeartbeat = () => sendOnline.mutate()
+        sendHeartbeat()
 
-    //     // Mark offline on tab close
-    //     const handleUnload = () => sendOffline.mutate()
-    //     window.addEventListener('beforeunload', handleUnload)
+        // Mark offline on tab close
+        const handleUnload = () => sendOffline.mutate()
+        window.addEventListener('beforeunload', handleUnload)
 
-    //     return () => {
-    //         window.removeEventListener('beforeunload', handleUnload)
-    //     }
-    // }, [user_organization])
+        return () => {
+            window.removeEventListener('beforeunload', handleUnload)
+        }
+
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [])
 
     if (!user_organization) {
-        toast.warning('Please select what org and branch to operate.')
         return <Navigate to={'/onboarding' as string} />
     }
 
