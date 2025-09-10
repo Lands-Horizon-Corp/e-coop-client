f849fa294b8cae6eae6f6b10c7e7843ff3c074e8
Jervx <louellagracechua@gmail.com>
2025-09-10 11:16:41 +0800

    chore: Add loan status combobox and modified form

--- src/modules/loan-status/components/forms/loan-status-create-update-form.tsx
+++ src/modules/loan-status/components/forms/loan-status-create-update-form.tsx
@@ -10,7 +10,6 @@ import IconCombobox from '@/components/comboboxes/icon-combobox'
 import FormFooterResetSubmit from '@/components/form-components/form-footer-reset-submit'
 import { TIcon } from '@/components/icons'
 import Modal, { IModalProps } from '@/components/modals/modal'
-import TextEditor from '@/components/text-editor'
 import { Form } from '@/components/ui/form'
 import FormFieldWrapper from '@/components/ui/form-field-wrapper'
 import { Input } from '@/components/ui/input'
@@ -28,6 +27,7 @@ import {
     LoanStatusSchema,
     TLoanStatusSchema,
 } from '../../loan-status.validation'
+import { Textarea } from '@/components/ui/textarea'
 
 export interface ILoanStatusFormProps
     extends IClassProps,
@@ -155,12 +155,11 @@ const LoanStatusCreateUpdateForm = ({
                             name="description"
                             label="Description"
                             render={({ field }) => (
-                                <TextEditor
+                                <Textarea
                                     {...field}
                                     content={field.value}
                                     placeholder="Description"
                                     disabled={isDisabled(field.name)}
-                                    textEditorClassName="bg-background"
                                 />
                             )}
                         />
