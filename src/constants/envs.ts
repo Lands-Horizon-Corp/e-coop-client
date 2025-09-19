fc852f386a041d0e0740bfc28f7f9cbe7dad6075
z-dayao <zalvendayao888@gmail.com>
2025-09-19 13:51:59 +0800

    chore: remove process env

--- src/constants/envs.ts
+++ src/constants/envs.ts
@@ -1,18 +1,12 @@
 // App Environment
-export const APP_ENV =
-    import.meta.env.VITE_APP_ENV || process?.env.VITE_APP_ENV || 'development'
+export const APP_ENV = import.meta.env.VITE_APP_ENV || 'development'
 
 // API / BACKEND SERVER
 export const API_URL =
-    import.meta.env.VITE_API_BASE_URL ||
-    process?.env.VITE_API_BASE_URL ||
-    'http://localhost:8000/api/'
+    import.meta.env.VITE_API_BASE_URL || 'http://localhost:8000/api/'
 
 // WS
-export const WS_URL =
-    import.meta.env.VITE_WS_URL ||
-    process?.env.VITE_WS_URL ||
-    'ws://localhost:8080'
+export const WS_URL = import.meta.env.VITE_WS_URL || 'ws://localhost:8080'
 
 // NATS
 export const NATS_CLIENT = import.meta.env.VITE_WS_CLIENT || ''
@@ -23,17 +17,10 @@ export const NATS_PASS = import.meta.env.VITE_WS_PASSWORD || ''
 
 // TURNSTILE CAPTCHA
 export const TURNSTILE_CAPTCHA_SITE_KEY =
-    import.meta.env.VITE_TURNSTILE_CAPTCHA_SITE_KEY ||
-    process?.env.VITE_TURNSTILE_CAPTCHA_SITE_KEY ||
-    ''
+    import.meta.env.VITE_TURNSTILE_CAPTCHA_SITE_KEY || ''
 
 // GOOGLE
 export const GOOGLE_MAPS_API_KEY =
-    import.meta.env.VITE_GOOGLE_MAPS_API_KEY ||
-    process?.env.VITE_GOOGLE_MAPS_API_KEY ||
-    ''
+    import.meta.env.VITE_GOOGLE_MAPS_API_KEY || ''
 
-export const GOOGLE_MAPS_MAP_ID =
-    import.meta.env.VITE_GOOGLE_MAPS_MAP_ID ||
-    process?.env.VITE_GOOGLE_MAPS_MAP_ID ||
-    ''
+export const GOOGLE_MAPS_MAP_ID = import.meta.env.VITE_GOOGLE_MAPS_MAP_ID || ''
--- vite.config.ts
+++ vite.config.ts
@@ -50,8 +50,4 @@ export default defineConfig({
         }
       }
     },
-    define: {
-    'process.env': process.env
-  }
-
 });
