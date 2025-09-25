bf002b82b9c9687016e2c7693210e0cd9565be30
dctrrohan <dctrrohan@gmail.com>
2025-09-25 15:32:42 +0800

    fix: merge error on company validations

--- src/modules/company/company.validation.ts
+++ src/modules/company/company.validation.ts
@@ -1,25 +1,16 @@
 import z from 'zod'
 
-<<<<<<< HEAD
-import { descriptionTransformerSanitizer, entityIdSchema } from '@/validation'
-=======
 import {
     descriptionSchema,
     descriptionTransformerSanitizer,
     entityIdSchema,
 } from '@/validation'
->>>>>>> 264d7b283 (feat: added module company)
 
 export const CompanySchema = z.object({
     name: z.string().min(1, 'Company name is required'),
-<<<<<<< HEAD
-    description: z
-        .string()
-=======
     media_id: entityIdSchema.optional(),
     media: z.any(),
     description: descriptionSchema
->>>>>>> 264d7b283 (feat: added module company)
         .min(10, 'Min 10 character description')
         .optional()
         .transform(descriptionTransformerSanitizer),
