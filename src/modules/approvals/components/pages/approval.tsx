543f6559c0916adb8d18ebd33910859c6124a41b
dctrrohan <dctrrohan@gmail.com>
2025-10-07 17:39:50 +0800

    feat: added signature on cash check voucher

--- src/modules/approvals/components/pages/approval.tsx
+++ src/modules/approvals/components/pages/approval.tsx
@@ -1,11 +1,32 @@
 import PageContainer from '@/components/containers/page-container'
+import {
+    ResizableHandle,
+    ResizablePanel,
+    ResizablePanelGroup,
+} from '@/components/ui/resizable'
 
 import Approval from '../approval'
 
 const ApprovalPage = () => {
     return (
-        <PageContainer className="">
-            <Approval className="min-h-[91dvh]" />
+        <PageContainer className="h-[100vh]">
+            <ResizablePanelGroup
+                direction="vertical"
+                className="w-full rounded-lg "
+            >
+                <ResizablePanel
+                    defaultSize={50}
+                    className="!overflow-auto h-fit"
+                >
+                    <Approval className="min-h-[91dvh] " />
+                </ResizablePanel>
+                <ResizableHandle withHandle />
+                <ResizablePanel defaultSize={50} className=" h-fit ">
+                    <div className="flex h-full items-center justify-center p-6">
+                        <span className="font-semibold">Content</span>
+                    </div>
+                </ResizablePanel>
+            </ResizablePanelGroup>
         </PageContainer>
     )
 }
