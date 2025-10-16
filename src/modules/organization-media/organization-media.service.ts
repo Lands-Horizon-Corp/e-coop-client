6c48008be37db704f109ac143983211789c64ca6
z-dayao <zalvendayao888@gmail.com>
2025-10-16 14:07:23 +0800

    feat: ading organization media

---
+++ src/modules/organization-media/organization-media.service.ts
@@ -0,0 +1,50 @@
+import { createDataLayerFactory } from '@/providers/repositories/data-layer-factory'
+
+
+import { Logger } from '@/helpers/loggers'
+
+import type { IOrganizationMedia, IOrganizationMediaRequest } from '../organization-media'
+
+const { apiCrudHooks, apiCrudService, baseQueryKey : organizationMediaBaseKey } = createDataLayerFactory<
+    IOrganizationMedia,
+    IOrganizationMediaRequest
+>({
+    url: '/api/v1/organization-media',
+    baseKey: 'organization-media',
+})
+
+// ⚙️🛠️ API SERVICE HERE
+export const {
+    API, // rarely used, for raw calls
+    route: organizationMediaAPIRoute, // matches url above
+
+    create: createOrganizationMedia,
+    updateById: updateOrganizationMediaById,
+
+    deleteById: deleteOrganizationMediaById,
+    deleteMany: deleteManyOrganizationMedia,
+
+    getById: getOrganizationMediaById,
+    getAll: getAllOrganizationMedia,
+    getPaginated: getPaginatedOrganizationMedia,
+} = apiCrudService
+
+// custom service functions can go here
+
+// 🪝 HOOK STARTS HERE
+export { organizationMediaBaseKey } // Exported in case it's needed outside
+
+export const {
+    useCreate: useCreateOrganizationMedia,
+    useUpdateById: useUpdateOrganizationMediaById,
+
+    useGetAll: useGetAllOrganizationMedia,
+    useGetById: useGetOrganizationMediaById,
+    useGetPaginated: useGetPaginatedOrganizationMedia,
+
+    useDeleteById: useDeleteOrganizationMediaById,
+    useDeleteMany: useDeleteManyOrganizationMedia,
+} = apiCrudHooks
+
+export const logger = Logger.getInstance('organization-media')
+// custom hooks can go here
