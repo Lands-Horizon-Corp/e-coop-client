519cd4501050caf7503c8ca3de702d504bcc9532
Jervx <louellagracechua@gmail.com>
2026-01-07 13:22:22 +0800

    chore: Add data layer for account transaction, account ntransaction entry

---
+++ src/modules/account-transaction-entry/account-transaction-entry.types.ts
@@ -0,0 +1,25 @@
+import z from 'zod'
+
+import { IBaseEntityMeta, IPaginatedResult, TEntityId } from '@/types'
+
+import { IAccountTransaction } from '../account-transaction/account-transaction.types'
+import { IAccount } from '../account/account.types'
+import { AccountTransactionEntrySchema } from './account-transaction-entry.validation'
+
+export interface IAccountTransactionEntry extends IBaseEntityMeta {
+    account_transaction_id: TEntityId
+    account_transaction: IAccountTransaction
+
+    account_id: TEntityId
+    account: IAccount
+
+    debit: number
+    credit: number
+}
+
+export type IAccountTransactionEntryRequest = z.infer<
+    typeof AccountTransactionEntrySchema
+>
+
+export interface IAccountTransactionEntryPaginated
+    extends IPaginatedResult<IAccountTransactionEntry> {}
---
+++ src/modules/account-transaction/account-transaction-constants.ts
@@ -0,0 +1,5 @@
+export const ACCOUNT_TRANSACTION_SOURCE = [
+    'daily_collection_book',
+    'cash_check_disbursement_book',
+    'general_journal',
+] as const
