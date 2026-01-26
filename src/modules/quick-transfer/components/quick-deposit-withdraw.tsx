82cf0727dd809b5687df4c842808b9eedadcd740
Jervx <louellagracechua@gmail.com>
2026-01-26 11:04:54 +0800

    chore: Add payment perm guard

--- src/modules/quick-transfer/components/quick-deposit-withdraw.tsx
+++ src/modules/quick-transfer/components/quick-deposit-withdraw.tsx
@@ -1,5 +1,6 @@
 import { toast } from 'sonner'
 
+import PermissionGuard from '@/modules/permission/components/permission-guard'
 // import { SHORTCUT_SCOPES } from '@/constants';
 import {
     CurrentTransactionWithdrawHistory,
@@ -30,19 +31,20 @@ const QuickDepositWithdraw = ({ mode }: { mode: TPaymentMode }) => {
         // <div onMouseOver={() => setActiveScope(SHORTCUT_SCOPES.QUICK_TRANSFER)}>
         <div>
             <PageContainer className="flex w-full !overflow-hidden">
-                <div className="flex w-full flex-col space-y-1">
-                    <div className="flex justify-start items-center space-x-2 w-full px-5">
-                        {mode === 'deposit' ? (
-                            <HandDepositIcon size={25} />
-                        ) : (
-                            <HandWithdrawIcon size={25} />
-                        )}
-                        <h4 className="scroll-m-20 text-xl font-semibold tracking-tight">
-                            {` Quick ${mode?.charAt(0).toUpperCase()}${mode?.slice(1)}`}
-                        </h4>
-                    </div>
-                    <div
-                        className={`
+                <PermissionGuard action={'Read'} resourceType="Transaction">
+                    <div className="flex w-full flex-col space-y-1">
+                        <div className="flex justify-start items-center space-x-2 w-full px-5">
+                            {mode === 'deposit' ? (
+                                <HandDepositIcon size={25} />
+                            ) : (
+                                <HandWithdrawIcon size={25} />
+                            )}
+                            <h4 className="scroll-m-20 text-xl font-semibold tracking-tight">
+                                {` Quick ${mode?.charAt(0).toUpperCase()}${mode?.slice(1)}`}
+                            </h4>
+                        </div>
+                        <div
+                            className={`
         mx-5 my-3 w-16 h-2 relative rounded-xl
         ${
             mode === 'withdraw'
@@ -51,51 +53,56 @@ const QuickDepositWithdraw = ({ mode }: { mode: TPaymentMode }) => {
         }
            before:absolute before:w-16 before:h-2 before:blur-sm before:top-1/2 before:left-1/2 before:-translate-x-1/2 before:-translate-y-1/2
       `}
-                    />
-                </div>
-                <ResizablePanelGroup
-                    className="!h-[80vh]"
-                    direction="horizontal"
-                >
-                    <ResizablePanel
-                        className=" !overflow-auto p-5 ecoop-scroll  "
-                        defaultSize={35}
-                        maxSize={40}
-                        minSize={0}
-                    >
-                        <QuickTransferTransactionForm
-                            mode={mode}
-                            onSuccess={() => {
-                                toast.success(
-                                    'Transaction completed successfully'
-                                )
-                            }}
-                            readOnly={!hasNoTransactionBatch}
                         />
-                    </ResizablePanel>
-                    <ResizableHandle withHandle />
-                    <ResizablePanel
-                        className="!overflow-y-auto px-5 ecoop-scroll !relative"
-                        defaultSize={65}
+                    </div>
+                    <ResizablePanelGroup
+                        className="!h-[80vh]"
+                        direction="horizontal"
                     >
-                        <div className="w-full flex items-center justify-end">
-                            <CurrentTransactionWithdrawHistory mode={mode} />
-                        </div>
-                        <div className="sticky top-0 z-50">
-                            <TransactionMemberProfile
-                                className="!bg-sidebar"
-                                memberInfo={selectedMember}
-                                viewOnly
+                        <ResizablePanel
+                            className=" !overflow-auto p-5 ecoop-scroll  "
+                            defaultSize={35}
+                            maxSize={40}
+                            minSize={0}
+                        >
+                            <QuickTransferTransactionForm
+                                mode={mode}
+                                onSuccess={() => {
+                                    toast.success(
+                                        'Transaction completed successfully'
+                                    )
+                                }}
+                                readOnly={!hasNoTransactionBatch}
                             />
-                        </div>
-                        <TransactionAccountMemberLedger
-                            memberProfileId={selectedMember?.id as TEntityId}
-                            onRowClick={(data) => {
-                                setSelectedAccount(data.original.account)
-                            }}
-                        />
-                    </ResizablePanel>
-                </ResizablePanelGroup>
+                        </ResizablePanel>
+                        <ResizableHandle withHandle />
+                        <ResizablePanel
+                            className="!overflow-y-auto px-5 ecoop-scroll !relative"
+                            defaultSize={65}
+                        >
+                            <div className="w-full flex items-center justify-end">
+                                <CurrentTransactionWithdrawHistory
+                                    mode={mode}
+                                />
+                            </div>
+                            <div className="sticky top-0 z-50">
+                                <TransactionMemberProfile
+                                    className="!bg-sidebar"
+                                    memberInfo={selectedMember}
+                                    viewOnly
+                                />
+                            </div>
+                            <TransactionAccountMemberLedger
+                                memberProfileId={
+                                    selectedMember?.id as TEntityId
+                                }
+                                onRowClick={(data) => {
+                                    setSelectedAccount(data.original.account)
+                                }}
+                            />
+                        </ResizablePanel>
+                    </ResizablePanelGroup>
+                </PermissionGuard>
             </PageContainer>
         </div>
     )
