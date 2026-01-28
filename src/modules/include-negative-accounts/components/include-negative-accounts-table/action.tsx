d09cf9f119ebf6bfb4c03ff27dcadde739c0f0ca
Jervx <louellagracechua@gmail.com>
2026-01-28 14:44:31 +0800

    feat(ABAC/negative-accounts): Added permission for negative accounts #125

--- src/modules/include-negative-accounts/components/include-negative-accounts-table/action.tsx
+++ src/modules/include-negative-accounts/components/include-negative-accounts-table/action.tsx
@@ -1,20 +1,25 @@
 import { ReactNode } from 'react'
 
 import { withToastCallbacks } from '@/helpers/callback-helper'
+import { hasPermissionFromAuth } from '@/modules/authentication/authgentication.store'
 import useConfirmModalStore from '@/store/confirm-modal-store'
 import { Row } from '@tanstack/react-table'
 
 import RowActionsGroup from '@/components/data-table/data-table-row-actions'
 import DataTableRowContext from '@/components/data-table/data-table-row-context'
-
-import { useModalState } from '@/hooks/use-modal-state'
+import { useTableRowActionStore } from '@/components/data-table/store/data-table-action-store'
 
 import { useDeleteIncludeNegativeAccountsById } from '../../include-negative-accounts.service'
 import { IIncludeNegativeAccounts } from '../../include-negative-accounts.types'
 import { IncludeNegativeAccountCreateUpdateFormModal } from '../forms/include-negative-account-create-update-form'
 import { IIncludeNegativeAccountTableActionComponentProp } from './columns'
 
-// ===== SHARED HOOK =====
+export type IncludeNegativeAccountActionType = 'edit'
+
+export interface IncludeNegativeAccountActionExtra {
+    onDeleteSuccess?: () => void
+}
+
 interface UseIncludeNegativeAccountActionsProps {
     row: Row<IIncludeNegativeAccounts>
     onDeleteSuccess?: () => void
@@ -24,8 +29,13 @@ const useIncludeNegativeAccountActions = ({
     row,
     onDeleteSuccess,
 }: UseIncludeNegativeAccountActionsProps) => {
-    const updateModal = useModalState()
     const item = row.original
+
+    const { open } = useTableRowActionStore<
+        IIncludeNegativeAccounts,
+        IncludeNegativeAccountActionType,
+        IncludeNegativeAccountActionExtra
+    >()
 
     const { onOpen } = useConfirmModalStore()
 
@@ -39,7 +49,13 @@ const useIncludeNegativeAccountActions = ({
             },
         })
 
-    const handleEdit = () => updateModal.onOpenChange(true)
+    const handleEdit = () => {
+        open('edit', {
+            id: item.id,
+            defaultValues: item,
+            extra: { onDeleteSuccess },
+        })
+    }
 
     const handleDelete = () => {
         onOpen({
@@ -51,7 +67,6 @@ const useIncludeNegativeAccountActions = ({
 
     return {
         item,
-        updateModal,
         isDeleting,
         handleEdit,
         handleDelete,
@@ -62,38 +77,37 @@ interface IncludeNegativeAccountActionProps extends IIncludeNegativeAccountTable
     onDeleteSuccess?: () => void
 }
 
-const IncludeNegativeAccountAction = ({
+export const IncludeNegativeAccountAction = ({
     row,
     onDeleteSuccess,
 }: IncludeNegativeAccountActionProps) => {
-    const { item, updateModal, isDeleting, handleEdit, handleDelete } =
+    const { item, isDeleting, handleEdit, handleDelete } =
         useIncludeNegativeAccountActions({ row, onDeleteSuccess })
 
     return (
-        <>
-            <div onClick={(e) => e.stopPropagation()}>
-                <IncludeNegativeAccountCreateUpdateFormModal
-                    {...updateModal}
-                    formProps={{
-                        includeNegativeAccountId: item.id,
-                        defaultValues: item,
-                    }}
-                />
-            </div>
-
-            <RowActionsGroup
-                onDelete={{
-                    text: 'Delete',
-                    isAllowed: !isDeleting,
-                    onClick: handleDelete,
-                }}
-                onEdit={{
-                    text: 'Edit',
-                    isAllowed: true,
-                    onClick: handleEdit,
-                }}
-            />
-        </>
+        <RowActionsGroup
+            onDelete={{
+                text: 'Delete',
+                isAllowed:
+                    !isDeleting &&
+                    hasPermissionFromAuth({
+                        action: ['Delete', 'OwnDelete'],
+                        resourceType: 'LoanSchemeIncludeNegativeAccounts',
+                        resource: item,
+                    }),
+                onClick: handleDelete,
+            }}
+            onEdit={{
+                text: 'Edit',
+                isAllowed: hasPermissionFromAuth({
+                    action: ['Update', 'OwnUpdate'],
+                    resourceType: 'LoanSchemeIncludeNegativeAccounts',
+                    resource: item,
+                }),
+                onClick: handleEdit,
+            }}
+            row={row}
+        />
     )
 }
 
@@ -107,36 +121,71 @@ export const IncludeNegativeAccountRowContext = ({
     children,
     onDeleteSuccess,
 }: IncludeNegativeAccountRowContextProps) => {
-    const { item, updateModal, isDeleting, handleEdit, handleDelete } =
+    const { item, isDeleting, handleEdit, handleDelete } =
         useIncludeNegativeAccountActions({ row, onDeleteSuccess })
 
     return (
-        <>
-            <IncludeNegativeAccountCreateUpdateFormModal
-                {...updateModal}
-                formProps={{
-                    includeNegativeAccountId: item.id,
-                    defaultValues: { ...item },
-                }}
-            />
+        <DataTableRowContext
+            onDelete={{
+                text: 'Delete',
+                isAllowed:
+                    !isDeleting &&
+                    hasPermissionFromAuth({
+                        action: ['Delete', 'OwnDelete'],
+                        resourceType: 'LoanSchemeIncludeNegativeAccounts',
+                        resource: item,
+                    }),
+                onClick: handleDelete,
+            }}
+            onEdit={{
+                text: 'Edit',
+                isAllowed: hasPermissionFromAuth({
+                    action: ['Update', 'OwnUpdate'],
+                    resourceType: 'LoanSchemeIncludeNegativeAccounts',
+                    resource: item,
+                }),
+                onClick: handleEdit,
+            }}
+            row={row}
+        >
+            {children}
+        </DataTableRowContext>
+    )
+}
 
-            <DataTableRowContext
-                onDelete={{
-                    text: 'Delete',
-                    isAllowed: !isDeleting,
-                    onClick: handleDelete,
-                }}
-                onEdit={{
-                    text: 'Edit',
-                    isAllowed: true,
-                    onClick: handleEdit,
-                }}
-                row={row}
-            >
-                {children}
-            </DataTableRowContext>
+export const IncludeNegativeAccountTableActionManager = () => {
+    const { state, close } = useTableRowActionStore<
+        IIncludeNegativeAccounts,
+        IncludeNegativeAccountActionType,
+        IncludeNegativeAccountActionExtra
+    >()
+
+    return (
+        <>
+            {state.action === 'edit' && state.defaultValues && (
+                <IncludeNegativeAccountCreateUpdateFormModal
+                    formProps={{
+                        includeNegativeAccountId: state.id,
+                        defaultValues: state.defaultValues,
+                    }}
+                    onOpenChange={close}
+                    open={state.isOpen}
+                />
+            )}
         </>
     )
 }
 
-export default IncludeNegativeAccountAction
+const IncludeNegativeAccountTableAction = ({
+    row,
+    onDeleteSuccess,
+}: IncludeNegativeAccountActionProps) => {
+    return (
+        <IncludeNegativeAccountAction
+            onDeleteSuccess={onDeleteSuccess}
+            row={row}
+        />
+    )
+}
+
+export default IncludeNegativeAccountTableAction
