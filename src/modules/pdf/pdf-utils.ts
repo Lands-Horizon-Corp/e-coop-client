2f5a9f2f6fb1ec8e05f061f64ff155d020ce90ee
Jervx <louellagracechua@gmail.com>
2026-03-12 14:44:56 +0800

    fix: Fix origin violation on print

--- src/modules/pdf/pdf-utils.ts
+++ src/modules/pdf/pdf-utils.ts
@@ -7,13 +7,33 @@ export const downloadPDF = (url: string, filename?: string) => {
     document.body.removeChild(link)
 }
 
-export const printPDF = (fileUrl: string) => {
+export const printPDF = async (fileUrl: string) => {
+    const res = await fetch(fileUrl)
+    const blob = await res.blob()
+
+    const downloadUrl = URL.createObjectURL(blob)
+
     const iframe = document.createElement('iframe')
     iframe.style.display = 'none'
-    iframe.src = fileUrl
+    iframe.src = downloadUrl
+
     document.body.appendChild(iframe)
+
     iframe.onload = () => {
         iframe.contentWindow?.print()
-        setTimeout(() => document.body.removeChild(iframe), 1000)
     }
+
+    // const iframe = document.createElement('iframe')
+    // iframe.style.display = 'none'
+    // iframe.src = fileUrl
+    // document.body.appendChild(iframe)
+    // iframe.onload = () => {
+    //     iframe.contentWindow?.print()
+    //     setTimeout(() => document.body.removeChild(iframe), 1000)
+    // }
+
+    // const win = window.open(fileUrl)
+    // win?.addEventListener('load', () => {
+    //     win.print()
+    // })
 }
